193ecdc4ec82b1e084ec16e6fcbce4510b83a87b
Rinat <rinsham55@gmail.com>
2024-02-09 00:06:29 +0300

    Add Less06

---
+++ Less06/src/main/java/org/s811286/sem6hw/Sem6hwApplication.java
@@ -0,0 +1,12 @@
+package org.s811286.sem6hw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sem6hwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sem6hwApplication.class, args);
+    }
+}
